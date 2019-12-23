e0cbb30c7b8f0bbfd4a6ae50849403a02dee32de
Junho Park <somedayif@hanyang.ac.kr>
2019-12-23 21:47:02 +0900

    Fix the controller
    inverse dynamics controller

--- dualarm_controller/include/Controller.cpp
+++ dualarm_controller/include/Controller.cpp
@@ -198,8 +198,8 @@ void Controller::InvDynController( VectorXd &_q, VectorXd &_qdot, VectorXd &_dq,
 	//FrictionCompensator(qdot, dqdot);
 
 	ToqOut.setZero();
-    //ToqOut =  K_Hinf.cwiseProduct( Kd.cwiseProduct(e_dev) + Kp.cwiseProduct(e)) + G ;
-    ToqOut =  M*( dqddot + K_Hinf.cwiseProduct( Kd.cwiseProduct(e_dev) + Kp.cwiseProduct(e)) )+ G ;
+    ToqOut =  K_Hinf.cwiseProduct( Kd.cwiseProduct(e_dev) + Kp.cwiseProduct(e)) + G ;
+    //ToqOut =  M*( dqddot + K_Hinf.cwiseProduct( Kd.cwiseProduct(e_dev) + Kp.cwiseProduct(e)) )+ G ;
 	Map<VectorXd>(p_Toq, this->m_Jnum) = ToqOut;
 	return;
 
